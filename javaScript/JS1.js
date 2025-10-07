6229adb2745874c6777c0ffca914a90b1e45eb4b
DESKTOP-E01N4IB\Jromper <jromper16@gmail.com>
2025-10-07 19:20:09 +0200

    Mejora del fondo y transicion con los botones

--- javaScript/JS1.js
+++ javaScript/JS1.js
@@ -1,3 +1,19 @@
-window.onclick = function() {
-    alert("Esto es una alerta");
-};
+function changeStyle() {
+  
+    document.body.classList.remove("cuerpo2");
+    document.getElementsByTagName("button")[1].classList.remove("boton2");
+
+    document.body.classList.add("cuerpo");
+    document.getElementsByTagName("button")[0].classList.add("boton");
+
+}
+
+function changeStyle2() {
+
+    document.body.classList.remove("cuerpo");
+    document.getElementsByTagName("button")[0].classList.remove("boton");
+
+    document.body.classList.add("cuerpo2");
+    document.getElementsByTagName("button")[1].classList.add("boton2");
+
+}
